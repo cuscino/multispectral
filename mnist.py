f6ac3cc9bf00008f7297272703393f9ad0417935
cuscino <luciano.sbaiz@gmail.com>
2020-05-07 18:57:02 +0000

    model mnist

--- mnist.py
+++ mnist.py
@@ -20,7 +20,6 @@ class MNISTModel(Model):
 
 def train_and_eval():
     mnist = tf.keras.datasets.mnist
-
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
